64aa686e12cb6dc02bdb31e3d98540a33351a34d
Lex <ap.public1@gmail.com>
2015-06-09 04:44:49 -0700

    Full path to php

--- filter.sh
+++ filter.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-php /www/config/ipfilter/filter.php /www/logs/hint.access.log /www/config/black_ips.json
+/opt/lampp/bin/php /www/config/ipfilter/filter.php /www/logs/hint.access.log /www/config/black_ips.json
